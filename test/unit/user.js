7a10f01825aa01decfd7a3631840f4a2c8f11e9d
Melanie Fryman <melanie@frymanet.com>
2014-08-25 12:26:29 -0500

    session

---
+++ test/unit/user.js
@@ -0,0 +1,41 @@
+/* jshint expr:true */
+/* global describe, it, before, beforeEach */
+
+'use strict';
+
+var expect    = require('chai').expect,
+    User    = require('../../app/models/User'),
+    dbConnect = require('../../app/lib/mongodb'),
+    cp        = require('child_process'),
+    db        = 'template-test';
+
+describe('User', function(){
+  before(function(done){
+    dbConnect(db, function(){
+      done();
+    });
+  });
+
+  beforeEach(function(done){
+    cp.execFile(__dirname + '/../scripts/clean-db.sh', [db], {cwd:__dirname + '/../scripts'}, function(err, stdout, stderr){
+      done();
+    });
+  });
+
+  describe('constructor', function(){
+    it('should create a new User object', function(){
+      var p = new User();
+      expect(p).to.be.instanceof(User);
+    });
+  });
+
+  describe('.all', function(){
+    it('should get all users', function(done){
+      User.all(function(err, users){
+        expect(users).to.have.length(2);
+        done();
+      });
+    });
+  });
+});
+
